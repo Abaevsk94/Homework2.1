f1e183ec711addbf86a9a8c7cfc128321ade72c5
Abaevsk94 <abaevsk@gmail.com>
2023-02-03 17:36:46 +0300

    Задание 3

--- src/Car.java
+++ src/Car.java
@@ -7,12 +7,12 @@ public class Car {
     String productionCountry;
 
     Car(String brand, String model, double engineVolume, String color, int productionYear, String productionCountry) {
-        this.brand = brand;
-        this.model = model;
-        this.engineVolume = engineVolume;
-        this.color = color;
-        this.productionYear = productionYear;
-        this.productionCountry = productionCountry;
+        this.brand = (brand == null || brand.isEmpty() ? "default" : brand);
+        this.model = (model == null || model.isEmpty() ? "default" : model);
+        this.engineVolume = (engineVolume <= 0 ? 1.5 : engineVolume);
+        this.color = (color == null || color.isEmpty() ? "белый" : color);
+        this.productionYear = (productionYear <= 0 ? 2000 : productionYear);
+        this.productionCountry = (productionCountry == null || productionCountry.isEmpty() ? "default" : productionCountry);
     }
 
     @Override
